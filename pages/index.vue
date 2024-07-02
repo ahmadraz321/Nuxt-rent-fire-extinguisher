6c367fcd7b620b33e764efa5524f7d41f0601ac2
Ahmad Raza <ahmad.ra880@gmail.com>
2024-07-02 12:58:01 +0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <HeroSection />
+      <div class="contain"></div>
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.contain {
+  height: 300px;
+}
+</style>
